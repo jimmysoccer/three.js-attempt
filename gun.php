0d0309d623b338c94781fa770f4550b07657152c
exit <jimmysoccer0927@gmail.com>
2021-04-15 16:17:11 +0800

    final adding

---
+++ gun.php
@@ -0,0 +1,124 @@
+
+<?php
+$nis = "hesun";
+$password= "092700Jimmy";
+$que = "WITH TOTS(CNT, D_ID) AS
+        (SELECT COUNT(C.C_ID), D.D_ID
+            FROM \"G.AGRAWAL\".\"CRIME\" C, \"G.AGRAWAL\".\"DATEVALUES\" D, \"G.AGRAWAL\".\"HAPPENS_AT\" H
+            WHERE C.C_ID = H.C_ID AND D.D_ID = H.D_ID AND D.D_YEAR>2016 AND D.D_YEAR<2021
+            GROUP BY D.D_ID),
+
+    GCASES(CNT, D_ID) AS
+        (SELECT COUNT(C.C_ID), D.D_ID
+            FROM \"G.AGRAWAL\".\"CRIME\" C, \"G.AGRAWAL\".\"DATEVALUES\" D, \"G.AGRAWAL\".\"HAPPENS_AT\" H, \"G.AGRAWAL\".\"ARCHIVE\" A
+            WHERE C.C_ID = H.C_ID AND D.D_ID = H.D_ID AND C.IUCR = A.IUCR AND D.D_YEAR>2016 AND D.D_YEAR<2021
+                AND (A.A_DESCRIPTION LIKE '%FIREARM%' OR A.A_DESCRIPTION LIKE '%HANDGUN%'
+                    OR A.A_DESCRIPTION LIKE '%AMMUNITION%' OR A.A_DESCRIPTION LIKE '%BULLET%'
+                    OR A.A_DESCRIPTION LIKE '%ARMED%' OR A.PRIMARY_TYPE LIKE '%WEAPONS%')
+            GROUP BY D.D_ID)
+
+SELECT TOTS.CNT AS \"TOTAL CASES\", GCASES.CNT AS \"GUN RELATED CASES\",
+    ROUND((GCASES.CNT/TOTS.CNT)*100,2) AS \"GUN RELATED CRIME RATE\", D.D_ID, D.D_MONTH, D.D_YEAR
+    FROM \"G.AGRAWAL\".\"DATEVALUES\" D, GCASES, TOTS
+    WHERE GCASES.D_ID = TOTS.D_ID AND TOTS.D_ID = D.D_ID
+    ORDER BY D.D_ID";
+$conn=oci_connect($nis,$password,
+'oracle.cise.ufl.edu:1521/orcl');
+if(empty($nis) or empty($password)){
+    echo "UserID atau Password is empty<br>\n";}
+
+if(!$conn){
+    echo 'connection error';
+}
+?>
+<html>
+<body>
+<h2>Query result</h2>
+<div align="center">
+<form method="get" action="homePage.php">
+    <button type="submit">HomePage</button>
+</form>
+</div>
+<?php
+$result=oci_parse($conn,$que);
+oci_execute($result);
+$gun=array();
+$rate=array();
+$total=array();
+while (($row = oci_fetch_array($result, OCI_NUM)) != false) {
+    $temp=$row[5] . $row[4];
+    $arrayName = array("label" =>"$temp" ,"y"=>$row[0]);
+    $arrayName1 = array("label" =>"$temp" ,"y"=>$row[1]);
+    $arrayName2 = array("label" =>"$temp" ,"y"=>$row[2]);
+    array_push($total,$arrayName);
+    array_push($gun,$arrayName1);
+    array_push($rate,$arrayName2);
+}
+oci_free_statement($result);
+oci_close($conn);
+?>
+</body>
+</html>
+<html>
+<head>
+<script>
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+  zoonEnabled:true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Gun Related Crime Cases"
+	},
+  axisX:{
+    title: "Year/Month"
+  }
+  ,
+	axisY: {
+		title: "Cases",
+    titleFontColor: "#4F81BC",
+		lineColor: "#4F81BC",
+		labelFontColor: "#4F81BC",
+		tickColor: "#4F81BC"
+	},
+  axisY2:{
+    title:"Rate",
+    titleFontColor: "#9ACD32",
+		lineColor: "#9ACD32",
+		labelFontColor: "#9ACD32",
+		tickColor: "#9ACD32"
+  },
+	data: [{
+		type: "line",
+    name:"total cases",
+    markerSize:0,
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($total, JSON_NUMERIC_CHECK); ?>
+	},{
+    type: "line",
+    name:"gun cases",
+		markerSize: 0,
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($gun, JSON_NUMERIC_CHECK); ?>
+  },{
+    type: "line",
+    name:"gun related crime rate",
+    axisYType:"secondary",
+    lineColor:"#9ACD32",
+		markerSize: 0,
+		showInLegend: true,
+		dataPoints: <?php echo json_encode($rate, JSON_NUMERIC_CHECK); ?>
+  }]
+});
+chart.render();
+}
+</script>
+</head>
+<body>
+<div id="chartContainer"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+
+</html>
--- homePage.php
+++ homePage.php
@@ -95,7 +95,7 @@
     </form>
     <form action="monthlyArrest.php">
       <button type="submit" style="margin:5px">
-        MONTHLY ARRESTS IN THE DISTRICT WITH HIGHEST CRIME</button>
+        Monthly Arrests In The District With Highest Crime</button>
     </form>
     <form action="apartmentVSstreet.php">
       <button type="submit" style="margin:5px">
@@ -108,5 +108,77 @@
       <button type="submit" style="margin:5px">
         Total Arrests VS Domestic Arrests</button>
     </form>
+    <form action="top5crime.php">
+      <button type="submit" style="margin:5px">
+        Top 5 Crimes Each Year</button>
+    </form>
+    <form action="gun.php">
+      <button type="submit" style="margin:5px">
+        Gun Related Crime Cases</button>
+    </form>
   </div>
+  <div class="form" align="center">
+    Total Labor Tuples:
+    <?php
+    $nis = "hesun";
+    $password= "092700Jimmy";
+    $que = "SELECT COUNT(*) AS \"TOTAL LABOR DATA\" FROM \"G.AGRAWAL\".\"LABOR_DATA\"";
+    $conn=oci_connect($nis,$password,
+    'oracle.cise.ufl.edu:1521/orcl');
+    if(empty($nis) or empty($password)){
+        echo "UserID atau Password is empty<br>\n";}
+
+    if(!$conn){
+        echo 'connection error';
+    }
+    $result=oci_parse($conn,$que);
+    oci_execute($result);
+    while (($row = oci_fetch_array($result, OCI_NUM)) != false) {
+        echo $row[0];
+    }
+    ?>
+  </div>
+
+    <div class="form" align="center">
+      Total Crime Tuples:
+      <?php
+      $nis = "hesun";
+      $password= "092700Jimmy";
+      $que = "SELECT COUNT(*) AS \"TOTAL CRIME DATA\" FROM \"G.AGRAWAL\".\"CRIME\"";
+      $conn=oci_connect($nis,$password,
+      'oracle.cise.ufl.edu:1521/orcl');
+      if(empty($nis) or empty($password)){
+          echo "UserID atau Password is empty<br>\n";}
+
+      if(!$conn){
+          echo 'connection error';
+      }
+      $result=oci_parse($conn,$que);
+      oci_execute($result);
+      while (($row = oci_fetch_array($result, OCI_NUM)) != false) {
+          echo $row[0];
+      }
+      ?>
+    </div>
+    <div class="form" align="center">
+      Total Covid Tuples:
+      <?php
+      $nis = "hesun";
+      $password= "092700Jimmy";
+      $que = "SELECT COUNT(*) AS \"TOTAL COVID DATA\" FROM \"G.AGRAWAL\".\"COVID_CASES\"";
+      $conn=oci_connect($nis,$password,
+      'oracle.cise.ufl.edu:1521/orcl');
+      if(empty($nis) or empty($password)){
+          echo "UserID atau Password is empty<br>\n";}
+
+      if(!$conn){
+          echo 'connection error';
+      }
+      $result=oci_parse($conn,$que);
+      oci_execute($result);
+      while (($row = oci_fetch_array($result, OCI_NUM)) != false) {
+          echo $row[0];
+      }
+      ?>
+    </div>
 </html>
